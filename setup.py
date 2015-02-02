549e55a4a3f47f7c46c247537778d30630236be0
Kon <bokarius@comcast.net>
2015-02-02 15:55:57 -0800

    updated dependency versions

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name='fanout',
-version='1.0.3',
+version='1.0.4',
 description='Fanout.io library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
 url='https://github.com/fanout/pyfanout',
 license='MIT',
 py_modules=['fanout'],
-install_requires=['pubcontrol>=2.0.0'],
+install_requires=['pubcontrol>=2,<3'],
 classifiers=[
 	'Topic :: Utilities',
 	'License :: OSI Approved :: MIT License'
