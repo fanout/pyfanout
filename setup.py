7f69ce7ac32544a2d48251c5b3ab1b790bed2f52
Kon <bokarius@comcast.net>
2015-04-14 13:39:35 -0700

    required latest pubcontrol lib

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
 name='fanout',
-version='1.1.0',
+version='1.1.1',
 description='Fanout.io library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
 url='https://github.com/fanout/pyfanout',
 license='MIT',
 py_modules=['fanout'],
-install_requires=['pubcontrol>=2.1.0,<3'],
+install_requires=['pubcontrol>=2.1.1,<3'],
 classifiers=[
 	'Topic :: Utilities',
 	'License :: OSI Approved :: MIT License'
