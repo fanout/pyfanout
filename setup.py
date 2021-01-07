98ed6ef71e9784f8e0c69cae86812c31a21c1157
Justin Karneges <justin@karneges.com>
2021-01-06 16:40:12 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 name='fanout',
-version='1.2.0',
+version='1.3.0',
 description='Fanout.io library',
 author='Justin Karneges',
 author_email='justin@fanout.io',
